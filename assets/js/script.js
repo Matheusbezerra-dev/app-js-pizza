c3ceec3bc575b52095058ded99d0adf95a612343
Matheus Bezerra <mabezerra2@gmail.com>
2022-09-29 16:54:15 -0400

    feat(PizzaWindowArea): fazendo alteração de opacidade
    fazendo modificação no codigo e colocando opacidade com setTimeout para ter uma pequena animação ao clicar em adcionar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,19 @@ const qS = (el) => document.querySelector(el);
 const qSAll = (el) => document.querySelectorAll(el);
 
 pizzaJson.map((pizza, i) => {
-  let pizzaItem = qS('.pizza-item').cloneNode(true);
-  pizzaItem.querySelector('.pizza-item--img img').src = pizza.img;
-  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
-  pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
-  pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
+  const pizzaItem = qS('.pizza-item').cloneNode(true);
+  const pizzaQS = (el) => pizzaItem.querySelector(el);  
+  pizzaQS('.pizza-item--img img').src = pizza.img;
+  pizzaQS('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
+  pizzaQS('.pizza-item--name').innerHTML = pizza.name;
+  pizzaQS('.pizza-item--desc').innerHTML = pizza.description;
+  pizzaQS('a').addEventListener(('click'), (e) => {
+    e.preventDefault();
+    qS('.pizzaWindowArea').style.opacity = 0;
+    qS('.pizzaWindowArea').style.display = 'flex';
+    setTimeout(() => {
+      qS('.pizzaWindowArea').style.opacity = 1;
+    }, 400)
+  });
   qS('.pizza-area').append(pizzaItem);
 });
